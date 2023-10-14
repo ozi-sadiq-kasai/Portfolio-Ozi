cd2037d6bbbd54e0999bba0181ad73b1949c7ef4
ozi <ozisadiqkasai@gmail.com>
2023-10-14 16:37:34 +0100

    modified files

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import diary from '../assets/Medic-Diary.png'
 import menu from '../assets/CBAE MENU.png'
+import apparel from "../assets/myapparel.png"
 import { AiOutlineGithub } from 'react-icons/ai'
 
 const Projects = () => {
@@ -33,10 +34,30 @@ const Projects = () => {
               <img src={menu} alt="" className="w-full" />
             </Link>
             <p className="bg-army text-light-army max-sm:p-2 text-center">
-              A restaurant menu powered by a CMS.
+              A restaurant menu powered by a CMS (Contentful).
             </p>
             <Link
               to="https://github.com/ozi-sadiq-kasai/react-cbae-menu"
+              target="_blank"
+            >
+              <AiOutlineGithub size={29} className="bg-army w-full py-1" />
+            </Link>
+          </li>
+
+          {/* Apparel */}
+          <li className="border-4 border-light-army max-sm:full m-auto mb-16 drop-shadow-xl rounded md:w-3/5">
+            <Link
+              to="https://my-apparel-101ahz9n7-infobakearoo-gmailcom.vercel.app/"
+              target="_blank"
+            >
+              <img src={apparel} alt="" className="w-full" />
+            </Link>
+            <p className="bg-army text-light-army max-sm:p-2 text-center">
+              A responsive clothing landing page built with React and Vanilla
+              css.
+            </p>
+            <Link
+              to="https://github.com/ozi-sadiq-kasai/react-cbae-menu/tree/main"
               target="_blank"
             >
               <AiOutlineGithub size={29} className="bg-army w-full py-1" />
